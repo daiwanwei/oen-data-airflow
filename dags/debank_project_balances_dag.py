6f2aefffee0a2860f61ecb871db06cc0c389d363
Wade <wade@teahouse.finance>
2023-11-11 22:13:36 +0800

    update the dag

--- dags/debank_project_balances_dag.py
+++ dags/debank_project_balances_dag.py
@@ -22,12 +22,11 @@ def debank_project_balances_dag():
 
     @task()
     def get_project_balances_task() -> list[str]:
-        # time_point = datetime.now()
-        # if abs(time_point.minute - 3) <= 1:
-        #     time_point = time_point.replace(minute=0, second=0, microsecond=0)
-        # else:
-        #     return []
         time_point = datetime.now()
+        if abs(time_point.minute - 3) <= 1:
+            time_point = time_point.replace(minute=0, second=0, microsecond=0)
+        else:
+            return []
         logging.info(f'execution date is {time_point}')
         file_paths = get_project_balances_task_logic(time_point, dir_path)
         return file_paths
--- dags/debank_user_balances_dag.py
+++ dags/debank_user_balances_dag.py
@@ -15,19 +15,18 @@ default_args = {
 }
 
 
-@dag(schedule_interval='3 * * * *', default_args=default_args)
+@dag(schedule_interval='15 * * * *', default_args=default_args)
 def debank_user_balances_dag():
     prefix = 'debank_user_balances'
     dir_path = './tmp_files'
 
     @task()
     def get_user_balances_task() -> list[str]:
-        # time_point = datetime.now()
-        # if abs(time_point.minute - 3) <= 1:
-        #     time_point = time_point.replace(minute=0, second=0, microsecond=0)
-        # else:
-        #     return []
         time_point = datetime.now()
+        if abs(time_point.minute - 15) <= 1:
+            time_point = time_point.replace(minute=0, second=0, microsecond=0)
+        else:
+            return []
         logging.info(f'execution date is {time_point}')
         file_paths = get_user_balances_task_logic(time_point, dir_path)
         return file_paths
